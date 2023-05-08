800d8fab152b8b8da04b02d06d97969e94d53c15
ArturXhoruxchyan <arturxoruxchyan@gmail.com>
2023-05-08 12:56:14 +0400

    homePage

--- src/pages/hompage/hompage.js
+++ src/pages/hompage/hompage.js
@@ -1,9 +1,27 @@
 import "./hompage.css"
+import React, { useState } from "react"
 
 export default function Home() {
-    return (
-        <>
-            <h1>Home Page</h1>
-        </>
-    )
-}
+    const [count, setCount] = useState(false)
+    const [nkar, setNkar] = useState(false)
+    const [real, setReal] = useState(false)
+    return <div>
+        <div className="masin">
+            <div className="as">
+                <div className="animals" onClick={() => setCount((e) => !e)}></div>
+                {count && <div className="real"><a className="a1" href="https://github.com/ArturXhoruxchyan/React_Project">Animals</a></div>}
+                </div>
+        
+
+            <div className="as">
+                <div className="animals1" onClick={() => setNkar((e) => !e)}></div>
+                {nkar && <div className="real"><a className="a1" href="https://github.com/ArturXhoruxchyan/React_Project">Animalse</a></div>}
+         </div>
+
+         <div className="as">
+                <div className="animals2" onClick={() => setReal((e) => !e)}></div>
+                {real && <div className="real"><a className="a1" href="https://github.com/ArturXhoruxchyan/React_Project">Animalsee</a></div>}
+         </div>
+    </div>
+    </div>
+    }
